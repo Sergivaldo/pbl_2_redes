7f3cdda6e94befa20e33d63c38644b51b059961f
Jacob Santana Almeida <jacobsantanaalmeida@gmail.com.br>
2023-03-26 22:24:32 -0300

    Class ClientMQTT

--- mqtt/client/src/main/java/br/uefs/Main.java
+++ mqtt/client/src/main/java/br/uefs/Main.java
@@ -1,7 +1,12 @@
 package br.uefs;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+        ClientMQTT clientMQTT = new ClientMQTT("EX: tcp://broker.mqttdashboard.com:1883", null, null);
+        clientMQTT.start();
+        Thread.sleep(1000);
+        String mensagem = "Teste de MQTT ";
+
+        clientMQTT.publish("teste", mensagem.getBytes(), 0);
     }
 }
